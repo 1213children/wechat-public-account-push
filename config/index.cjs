3ae014416a0005a2bf0277bb3f280620d35cb17d
Zehao Shen <85060531+1213children@users.noreply.github.com>
2023-05-22 17:14:13 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o1wNx6der6cA5Z-UZ-JJcz_lrkxQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ZUipBzCcXAcyI7_OelCi9FJJzC7GEhtCAtXlPuynapdk',
+      useTemplateId: 'UipBzCcXAcyI7_OelCi9FJJzC7GEhtCAtXlPuynapdk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-22',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'Z3F_7XjdOtuGmJWGvnOSOU9kdUko9UlB1Yo4JYF9xBI',
+  CALLBACK_TEMPLATE_ID: '	UipBzCcXAcyI7_OelCi9FJJzC7GEhtCAtXlPuynapdk',
 
   CALLBACK_USERS: [
     {
